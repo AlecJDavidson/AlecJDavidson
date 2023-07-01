dc8a55417ddb4427aaf6ddbcfc8912f3002f73aa
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-01 17:29:26 -0400

    First section of home screen content looking pretty good.

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -7,6 +7,10 @@ const Home = () => {
       <p className='second-line'>Alec Davidson.</p>
       <p className='third-line'>The tower of power,</p>
       <p className='third-line'>too sweet to be sour!</p>
+      <p className='bio'>
+        I'm a Software Engineer based in Indianapolis. I specialize in creating
+        websites and app solutions in the cloud to transform your business.
+      </p>
     </div>
   )
 }
