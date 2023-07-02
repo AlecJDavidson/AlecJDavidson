9a42eef1e277b9c4cc93cb63c25bd2fb639726b9
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-01 22:00:09 -0400

    Chakra is pretty neat. Nav is coming along.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import 'react'
 import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import Home from './views/home/Home'
-import Navbar from './components/ui-components/navbar/Navbar'
+import Nav from './components/ui-components/navbar/Nav'
 
 const App = () => {
   return (
-    <>
-      <Navbar/>
+    <ChakraProvider>
+      <Nav/>
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
-    </>
+    </ChakraProvider>
   )
 }
 
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,16 +2,7 @@ import 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div className='home-content'>
-      <p className='first-line'>Hi, my name is</p>
-      <p className='second-line'>Alec Davidson.</p>
-      <p className='third-line'>Tower of power, too sweet to be sour!</p>
-      <p className='bio'>
-        I'm a Software Engineer based in Indianapolis. I specialize in creating
-        websites and app solutions in the cloud to transform your business.
-      </p>
-    </div>
-  )
+<>content here</> )
 }
 
 export default Home
