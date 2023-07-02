67b7df1a98cade03a2a44d73f77de5338aa11792
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-02 15:05:03 -0400

    Hero component is looking better. Need to work on centering and how it renders on mobile.

--- src/components/ui-components/hero/Hero.tsx
+++ src/components/ui-components/hero/Hero.tsx
@@ -1,29 +1,59 @@
-
-import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import React from 'react'
+import { Box, Heading, Text, VStack } from '@chakra-ui/react'
 
 interface HeroStatementProps {
-  hello: string;
-  title: string;
-  subtitle: string;
-  buttonText: string;
+  hello: string
+  title: string
+  title2: string
+  subtitle: string
 }
 
 const HeroStatement: React.FC<HeroStatementProps> = ({
+  hello,
   title,
+  title2,
   subtitle,
 }) => {
   return (
-    <Box p={8} bg="gray.800" color="white">
-      <Text>Hi, my name is</Text>
-      <Heading as="h1" size="2xl" mb={4}>
-        {title}
-      </Heading>
-      <Text fontSize="xl" mb={6}>
-        {subtitle}
-      </Text>
+    <Box
+      p={8}
+      bg='gray.800'
+      color='white'
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+      textAlign='left'
+    >
+      <VStack spacing={4} alignItems='flex-start'>
+        <Text fontSize={'1rem'}  maxWidth='30rem' textAlign='left'>
+          {hello}
+        </Text>
+        <Heading
+          as='h1'
+          size='2xl'
+          fontSize='4.375rem'
+          fontWeight='bolder'
+          textAlign='left'
+          maxWidth='40rem'
+        >
+          {title}
+        </Heading>
+        <Heading
+          as='h1'
+          size='2xl'
+          fontSize='4.375rem'
+          textAlign='left'
+          maxWidth='45rem'
+        >
+          {title2}
+        </Heading>
+        <Text fontSize='1.25rem' mb={6} textAlign='left' maxWidth='35rem' margin={'1.5rem 0px'}>
+          {subtitle}
+        </Text>
+      </VStack>
     </Box>
-  );
-};
+  )
+}
 
-export default HeroStatement;
+export default HeroStatement
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,14 @@
 import 'react'
 import HeroStatement from '../../components/ui-components/hero/Hero'
 const Home = () => {
-  return <HeroStatement hello='Hi, my name is' title='Alec Davidson' subtitle='test' buttonText='test' />
+  return (
+    <HeroStatement
+      hello='Hi, my name is'
+      title='Alec Davidson.'
+      title2='The tower of power, too sweet to be sour!'
+      subtitle='test'
+    />
+  )
 }
 
 export default Home
