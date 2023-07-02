67642cd2558bae17f97ddd6fd08b8f5f0fd19d89
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-01 20:22:30 -0400

    Got the landing page looking how I want. Now I need to make it responsive.

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,8 +5,7 @@ const Home = () => {
     <div className='home-content'>
       <p className='first-line'>Hi, my name is</p>
       <p className='second-line'>Alec Davidson.</p>
-      <p className='third-line'>The tower of power,</p>
-      <p className='third-line'>too sweet to be sour!</p>
+      <p className='third-line'>Tower of power, too sweet to be sour!</p>
       <p className='bio'>
         I'm a Software Engineer based in Indianapolis. I specialize in creating
         websites and app solutions in the cloud to transform your business.
