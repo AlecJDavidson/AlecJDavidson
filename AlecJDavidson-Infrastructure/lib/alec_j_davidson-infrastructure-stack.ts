d63c21abfdbc9fc81e144527f2ba401bdd48c0fe
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-04 16:33:02 -0400

    Added removal policy.

--- AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
+++ AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
@@ -1,27 +1,24 @@
-import { Stack, StackProps, App } from 'aws-cdk-lib';
-import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { AnyPrincipal } from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps, App, RemovalPolicy } from 'aws-cdk-lib'
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3'
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
 
 export class AlecJDavidsonInfrastructureStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Create an S3 bucket to host alecjdavidson.com
     const bucket = new Bucket(this, 'AlecJDavidsonBucket', {
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
       publicReadAccess: true,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ALL, // Enable blocking public access to prevent editing, deleting, or adding files
-    });
-
-    // Grant public read access to the objects in the bucket
-    bucket.grantRead(new AnyPrincipal());
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // Disable blockPublicPolicy and blockPublicAcls
+      removalPolicy: RemovalPolicy.DESTROY, // Optional: Set the bucket removal policy
+    })
 
     // Deploy the build files to the S3 bucket
     new BucketDeployment(this, 'AlecJDavidsonDeployment', {
       sources: [Source.asset('../dist')],
       destinationBucket: bucket,
-    });
+    })
   }
 }
