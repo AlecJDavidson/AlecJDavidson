40514a8a3b9530dbf1b37672ba01f3ad710535ed
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-04 18:05:44 -0400

    Edited access policies.

--- AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
+++ AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
@@ -1,27 +1,26 @@
 import { Stack, StackProps, App } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { AnyPrincipal } from 'aws-cdk-lib/aws-iam';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 export class AlecJDavidsonInfrastructureStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Create an S3 bucket to host alecjdavidson.com
     const bucket = new Bucket(this, 'AlecJDavidsonBucket', {
       websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
       publicReadAccess: true,
-      //blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // Enable blocking public access to prevent editing, deleting, or adding files
-    });
-
-    // Grant public read access to the objects in the bucket
-    bucket.grantRead(new AnyPrincipal());
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
 
     // Deploy the build files to the S3 bucket
     new BucketDeployment(this, 'AlecJDavidsonDeployment', {
       sources: [Source.asset('../dist')],
       destinationBucket: bucket,
-    });
+    })
   }
 }
+
+
