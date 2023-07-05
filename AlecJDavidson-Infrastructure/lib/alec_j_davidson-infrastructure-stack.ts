15be53a65780b83db0addbff0426cc6cc54bce8a
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-04 23:51:54 -0400

    Appended CDK Template.

--- AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
+++ AlecJDavidson-Infrastructure/lib/alec_j_davidson-infrastructure-stack.ts
@@ -1,9 +1,10 @@
 import { Stack, StackProps, App } from 'aws-cdk-lib'
-import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3'
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
+import { AnyPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { RemovalPolicy } from 'aws-cdk-lib'
 
-export class AlecJDavidsonInfrastructureStack extends Stack {
+export class AlecJDavidsonWebsiteInfrastructureStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
 
@@ -11,13 +12,28 @@ export class AlecJDavidsonInfrastructureStack extends Stack {
     const bucket = new Bucket(this, 'AlecJDavidsonBucket', {
       websiteIndexDocument: 'index.html',
       publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // Set bucket access control to private
       removalPolicy: RemovalPolicy.DESTROY,
     })
 
+    // Add bucket policy allowing public read access to objects
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [`${bucket.bucketArn}/www.alecjdavidson.com/*`],
+        principals: [new AnyPrincipal()],
+        effect: Effect.ALLOW,
+      })
+    )
+
     // Deploy the build files to the S3 bucket
     new BucketDeployment(this, 'AlecJDavidsonDeployment', {
-      sources: [Source.asset('../dist')],
+      sources: [Source.asset('../dist')], // Update the path to your build directory
       destinationBucket: bucket,
     })
   }
 }
+
+const app = new App()
+new AlecJDavidsonWebsiteInfrastructureStack(app, 'AlecJDavidsonInfrastructureStack')
+app.synth()
