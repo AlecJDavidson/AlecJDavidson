4a89d35ce2834a2a689257e18eecce6365ac2ada
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-06 12:21:11 -0400

    Posting to contact lambda

--- src/components/ui-components/contact/logic/postNote.ts
+++ src/components/ui-components/contact/logic/postNote.ts
@@ -2,8 +2,9 @@ import { Note } from '../types/note'
 
 const postNote = async (note: Note) => {
   try {
-    const testURL = 'https://webhook.site/baa23c96-13d4-477d-a2e3-4842fb75564a'
-    const response = await fetch(testURL, {
+    // const testURL = 'https://webhook.site/baa23c96-13d4-477d-a2e3-4842fb75564a'
+    const contactLambdaUrl = 'https://wms3gdk4bqdzmjw36k2wxgjr7a0rjtcf.lambda-url.us-east-1.on.aws/'
+    const response = await fetch(contactLambdaUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
